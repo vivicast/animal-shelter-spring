a5933ffe3c2dca2f346c7d8a5e05fea767650f3d
Viviana Castillo <v.castillo@alumnos.upm.es>
2021-05-08 12:25:46 +0200

    Test mongoDB, save an adopter in mongoDB local #8

--- src/main/java/es/animal/protection/animalshelter/AnimalShelterApplication.java
+++ src/main/java/es/animal/protection/animalshelter/AnimalShelterApplication.java
@@ -1,13 +1,42 @@
 package es.animal.protection.animalshelter;
 
+import es.animal.protection.animalshelter.infrastructure.mongodb.daos.AdopterReactive;
+import es.animal.protection.animalshelter.infrastructure.mongodb.entities.AdopterEntity;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import reactor.core.publisher.Flux;
 
 @SpringBootApplication
 public class AnimalShelterApplication {
+    private Logger log = LoggerFactory.getLogger(this.getClass());
 
-	public static void main(String[] args) {
-		SpringApplication.run(AnimalShelterApplication.class, args);
-	}
+    public static void main(String[] args) {
+
+        SpringApplication.run(AnimalShelterApplication.class, args);
+    }
+
+    /**
+     * Usamos CommandLineRunner para iniciar nuestro flujo de trabajo.
+     * El CommandLineRunner es una interfaz call back en Spring Boot,
+     * cuando nuestra aplicación arranca llamará al método start y le
+     * pasará los argumentos unsando el método interno run()
+     */
+    @Bean
+    CommandLineRunner start(AdopterReactive adopterReactive) {
+        return args -> {
+
+            Flux.just(
+                    AdopterEntity.builder().name("Mary Smith").address("Av. Glory").birthDay("1999/12/09").nif("50645263F").build(),
+                    AdopterEntity.builder().name("John Smith").address("Av. Hollywood").birthDay("1995/28/01").nif("000000F").build()
+            )
+                    .flatMap(adopterReactive::save)
+                    .subscribe(person -> log.info("person: {}", person));
+
+        };
+    }
 
 }
--- src/test/java/es/animal/protection/animalshelter/AnimalShelterApplicationTests.java
+++ src/test/java/es/animal/protection/animalshelter/AnimalShelterApplicationTests.java
@@ -1,5 +1,6 @@
 package es.animal.protection.animalshelter;
 
+import es.animal.protection.animalshelter.infrastructure.mongodb.daos.AdopterReactive;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -9,7 +10,7 @@ class AnimalShelterApplicationTests {
 
 	@Test
 	void contextLoads() {
-		String hello = "Hello Worlds";
+		String hello = "Hello World";
 		HelloWorld helloWorld = new HelloWorld();
 		Assertions.assertEquals(hello, helloWorld.home());
 	}

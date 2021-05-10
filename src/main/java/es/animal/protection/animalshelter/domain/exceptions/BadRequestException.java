bc355ca5a77ddfd96835f50e8ddbb275dfa32303
Viviana Castillo <v.castillo@alumnos.upm.es>
2021-05-10 22:19:45 +0200

    Exception handler #2

---
+++ src/main/java/es/animal/protection/animalshelter/domain/exceptions/BadRequestException.java
@@ -0,0 +1,10 @@
+package es.animal.protection.animalshelter.domain.exceptions;
+
+public class BadRequestException extends RuntimeException {
+    private static final String DESCRIPTION = "Bad Request Exception";
+
+    public BadRequestException(String detail) {
+        super(DESCRIPTION + ". " + detail);
+    }
+
+}
---
+++ src/main/java/es/animal/protection/animalshelter/domain/exceptions/NotFoundException.java
@@ -0,0 +1,10 @@
+package es.animal.protection.animalshelter.domain.exceptions;
+
+public class NotFoundException extends RuntimeException {
+    private static final String DESCRIPTION = "Not Found Exception";
+
+    public NotFoundException(String detail) {
+        super(DESCRIPTION + ". " + detail);
+    }
+
+}

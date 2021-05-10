ce61e8005b0a4e7c87519224370be61c7c833fe7
Viviana Castillo <v.castillo@alumnos.upm.es>
2021-05-10 22:25:42 +0200

    Add constructor without arguments and add identifier  #2

--- src/main/java/es/animal/protection/animalshelter/infrastructure/mongodb/entities/AdopterEntity.java
+++ src/main/java/es/animal/protection/animalshelter/infrastructure/mongodb/entities/AdopterEntity.java
@@ -4,17 +4,20 @@ import es.animal.protection.animalshelter.domain.model.Adopter;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.beans.BeanUtils;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 @Document
 @Builder
 public class AdopterEntity {
-    private String name;
     @Id
+    private String id;
+    private String name;
     private String nif;
     private String address;
     private String birthDay;
